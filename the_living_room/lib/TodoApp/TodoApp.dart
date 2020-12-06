2e3cbf7be693fbd9f03b0ecfae74cab861dfb010
umarz14 <martinezuriel2016@gmail.com>
2020-12-05 17:38:20 -0800

    fix merge issues

--- the_living_room/lib/TodoApp/TodoApp.dart
+++ the_living_room/lib/TodoApp/TodoApp.dart
@@ -1,7 +1,11 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
-import 'to_do_card.dart';
-import 'todo.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:the_living_room/TodoApp/Cards.dart';
+import 'package:the_living_room/TodoApp/Classes.dart';
+import 'package:the_living_room/TodoApp/addTask.dart';
+
 
 class ToDoApp extends StatelessWidget {
   @override
@@ -12,6 +16,102 @@ class ToDoApp extends StatelessWidget {
     );
   }
 }
+/*
+Future<User> _fetchUserInfo(String id) async {
+  User fetchedUser;
+  var snapshot = await FirebaseFirestore.instance
+      .collection('user')
+      .doc(id)
+      .get();
+  return User(snapshot);
+}
+*/
+
+/*
+foo() async {
+  final id = FirebaseAuth.instance.currentUser.uid;
+  final user = await getdata1(id);
+  return await getdata1(id);
+
+}
+
+getdata1(String id) async{
+  // This is the uid for their User Document
+  String hid = '';
+  final id = FirebaseAuth.instance.currentUser.uid;
+  print(id);
+
+  await FirebaseFirestore.instance
+      .collection('users')
+      .doc(id)
+      .get()
+      .then((DocumentSnapshot documentSnapshot) {
+    if (documentSnapshot.exists) {
+      //print('Document exists on the database');
+      Map<String, dynamic> data = documentSnapshot.data();
+      //print("{$data['household']}");
+      return (data["household"].toString());
+      print("hid = $hid");
+    }});
+  return hid;
+}
+
+*/
+
+/*
+final String _collection = 'users';
+final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+getData() async {
+  return await _fireStore.collection(_collection).doc('h5e1Q7DKMfPwpccjfYuxSgfOF0W2').get();
+}
+getDat() {
+  getData().then((val) {
+    if (val.documents.length > 0) {
+      print(val.documents[0].data["household"]);
+    }
+    else {
+      print("Not Found");
+    }
+  });
+}
+
+ */
+
+class TaskList extends StatelessWidget {
+
+  final id = FirebaseAuth.instance.currentUser.uid;
+  //final hid  = foo();
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference tasks = FirebaseFirestore.instance.collection('household').doc(id).collection('tasks');
+    return StreamBuilder<QuerySnapshot>(
+      stream: tasks.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return new ListView(
+          children: snapshot.data.docs.map((DocumentSnapshot document) {
+            return new ToDoCard(
+              toDoItem: ToDoItem(todo:document.data()['task'], creator: document.data()['creator'], delegated:document.data()['delegated'], id:document.id),
+              delete: () {
+                tasks.doc(document.id)
+                    .delete();
+              },
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
 
 class ToDoList extends StatefulWidget {
   @override
@@ -19,67 +119,8 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
-  // Variables
-  // Create a text controller. Later, use it to retrieve the
-  // current value of the TextField.
-  final myController = TextEditingController();
-
-  //List<String> tdl = List<String>(); // TO DO LIST
-  List<ToDoItem> tdl = [
-    ToDoItem(todo: 'go to store', user: 'me'),
-    ToDoItem(todo: 'say hello world', user:'me'),
-    ToDoItem(todo: 'update list', user: 'me')
-  ];
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is removed from the
-    // widget tree.
-    myController.dispose();
-    super.dispose();
-  } // End of Dispose
-
-  void addTodoItem(String tdi) {
-
-    String nameOfCreator = 'me';
-    final auth.User currentUser = auth.FirebaseAuth.instance.currentUser;
-    if (currentUser != null) {
-      nameOfCreator = currentUser.displayName;
-    } // Enf of if
-    else{
-      nameOfCreator = "user = null";
-    } // End of else
-
-    setState(() {
-      tdl.add(ToDoItem(todo: tdi, user: nameOfCreator));
-    });
-  } // End of addTodoItem
-
-  // This function creates a new screen in top of our current screen where
-  // one can add items
-  void pushAddTodoScreen() {
-    //push page onto the stack; yes stack literally a a stack
-    Navigator.of(context).push(
-        // MaterialapageRoute automatically animates a screen entry
-        // We will also use this page to a back button to close itself(does itself)
-        MaterialPageRoute(builder: (context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('New Task'),
-        ),
-        body: TextField(
-          decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(16),
-              hintText: 'What do you have to do?'),
-          onSubmitted: (toDoTask) {
-            addTodoItem(toDoTask);
-            Navigator.pop(context); // Close the New Task Screen
-          },
-        ),
-      );
-    }));
-  }
-
+  final id = FirebaseAuth.instance.currentUser.uid;
+  //String hid = foo();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,23 +128,26 @@ class _ToDoListState extends State<ToDoList> {
         title: Text('Todo List'),
         centerTitle: true,
       ),
-      body: ListView.builder(
-          itemCount: tdl.length,
-          itemBuilder: (context, index) {
-            return ToDoCard(
-              toDoItem: tdl[index],
-              delete: () {
-                setState(() {
-                  tdl.remove(tdl[index]);
-                });
-              },
-            );
-          }),
+      body: TaskList(),
       floatingActionButton: FloatingActionButton(
-        onPressed: pushAddTodoScreen,
+        onPressed: () {
+
+
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => PushAddToDoScreen())
+          );
+        },
+
+        //String hello = "apple";
+        //print('household $hello');
+        //hello = getHouseId();
+        //print('household $hello');
+        //String y = getdata1();
+        //print("pritint outisde the function $y");
         tooltip: 'Add Task',
         child: Icon(Icons.add),
       ),
     );
   } // End of build
 }
+
